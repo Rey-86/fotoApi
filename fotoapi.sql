98dc68e8fe2e35f452668cf085822264dbc85596
jtecoia <jtecoia@gmail.com>
2023-02-28 19:12:11 +0100

    modificado funcion listaFotos para que muestre el numero de votos de cada foto. Modificada base de datos fotoapi.sql

--- fotoapi.sql
+++ fotoapi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2023 a las 17:39:11
+-- Tiempo de generación: 28-02-2023 a las 19:08:37
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.1.12
 
@@ -32,11 +32,22 @@ USE `fotoapi`;
 DROP TABLE IF EXISTS `fotos`;
 CREATE TABLE `fotos` (
   `idfoto` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
+  `fecha` date DEFAULT current_timestamp(),
   `titulo` varchar(255) DEFAULT NULL,
   `archivo` varchar(255) DEFAULT NULL,
   `idusuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `fotos`
+--
+
+INSERT INTO `fotos` (`idfoto`, `fecha`, `titulo`, `archivo`, `idusuario`) VALUES
+(1, '2023-02-27', 'Foto paisaje', 'paisaje.jpg', 1),
+(3, '2023-02-27', 'mesa', 'mesa.jpg', 9),
+(4, '2023-02-28', 'casa', 'casa.jpg', 9),
+(5, '2023-02-28', 'robot', 'robot.jpg', 1),
+(6, '2023-02-28', 'mujer', 'mujer.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -52,6 +63,16 @@ CREATE TABLE `usuarios` (
   `password` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `nombre`, `email`, `password`) VALUES
+(1, 'jesus', 'jesus@gmail.com', '1234'),
+(9, 'jesus2', 'jesus2@gmail.com', '1234'),
+(10, 'Pedro', 'pedro@dge.com', '1234'),
+(14, 'Luis', 'luis@dge.com', '1234');
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +84,17 @@ CREATE TABLE `votos` (
   `idusuario` int(11) NOT NULL,
   `idfoto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `votos`
+--
+
+INSERT INTO `votos` (`idusuario`, `idfoto`) VALUES
+(1, 1),
+(1, 3),
+(1, 4),
+(9, 1),
+(9, 3);
 
 --
 -- Índices para tablas volcadas
@@ -97,13 +129,13 @@ ALTER TABLE `votos`
 -- AUTO_INCREMENT de la tabla `fotos`
 --
 ALTER TABLE `fotos`
-  MODIFY `idfoto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idfoto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+//Verificação com o botão Login
+if(isset($_POST['btn-primary'])):
+    require_once('conectar.php');
+    $usuario=$_POST['usuario'];
+    $pass=$_POST['password'];
+    $datos=new Datos();
+    if($datos->login($usuario,$pass)){
+    //Iniciar session
+    session_start();
+
+        header('Location: index.php');
+
+        exit();
+    }else{
+        //Mostrar mensaje de error
+        $msj='Usuario o contraseña incorrecta';
+     
+    }
+
+endif;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Foto-Api</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container col-md-6 col-sm-12">
+        <form action="subirfoto.php" method="post">
+            <div class="form-group">
+                <label for="usuario">Usuario</label>
+                <input class="form-control col-md-6 col-sm-12" type="text" name="usuario" id="usuario">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control col-md-6 col-sm-12" type="text" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <button type="submit" name="btn-primary">Entrar</button>
+            </div>
+            <div class="form-group col-md-6 col-sm-12">
+                <p>Si todavía no estás registrado pulse <a href="registrarse.html">aquí</a>: </p>
+        </form>
+        <?php if(isset($msj)) echo $msj; ?>
+    </div>
+</body>
+
+</html>
