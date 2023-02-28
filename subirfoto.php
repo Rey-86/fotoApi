36e70ffbf90f1b91fbdd754acd7c11c1147ddeed
Rey-86 <reygranhacristian@gmail.com>
2023-02-28 20:27:59 +0100

    reparar insertar en base de datos

--- subirfoto.php
+++ subirfoto.php
@@ -1,94 +1,55 @@
+<?php
+    if (isset($_FILES['pic'])) {
+      $ext = strtolower(substr($_FILES['pic']['name'], -4)); //Pegando extensão do arquivo
+      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+      $dir = './imagens/'; //Diretório para uploads
+
+      move_uploaded_file($_FILES['pic']['tmp_name'], $dir . $new_name); //Fazer upload do arquivo
+
+      $titulo = $_POST["titulo"];
+      $archivo = $new_name;
+      $idusuario = 1;
+
+      require_once("conectar.php");
+      $dato = new Datos();
+      $listafotos=$dato->subirFoto($titulo, $archivo, $idusuario);
+      header("Location: index.php");
+  exit();
+    }
+
+
+    ?>
+
 <html>
+
 <head>
- <title>Upload de imagens</title>
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <title>Upload de imagens</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 
 <body>
-<div class="container">
-<h2><strong>Envio de imagens</strong></h2><hr>
+  <div class="container">
+    <h2><strong>Envio de imagens</strong></h2>
+    <hr>
 
-<form method="POST" enctype="multipart/form-data" action="index.php">
-  <label for="conteudo">Enviar imagem:</label>
-  <input type="file" name="pic" accept="image/*" class="form-control">
+    <form method="POST" enctype="multipart/form-data" action="">
+      <label for="conteudo">Enviar imagem:</label>
+      <input type="file" name="pic" accept="image/*" class="form-control">
+      <input type="text" name="titulo" class="form-control" placeholder="Título de la foto">
+      <div>
+        <button type="submit" class="btn btn-success">Enviar imagem</button>
+      </div>
+    </form>
 
-  <div align="center">
-    <button type="submit" class="btn btn-success">Enviar imagem</button>
+    <hr>
+    
   </div>
-</form>
- 
- <hr>
- <?php
- if(isset($_FILES['pic']))
- {
-    $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
-    $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-    $dir = './imagens/'; //Diretório para uploads
- 
-    move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
-    
- } 
 
- 
- ?>
- 
- </div>
-<body>
-</html>
-<html>
-<head>
- <title>Upload de imagens</title>
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-</head>
+</body>
 
-<body>
-<div class="container">
-<h2><strong>Envio de imagens</strong></h2><hr>
-
-<form method="POST" enctype="multipart/form-data" action="index.php">
-  <label for="conteudo">Enviar imagem:</label>
-  <input type="file" name="pic" accept="image/*" class="form-control">
-
-  <div>
-    <button type="submit" class="btn btn-success">Enviar imagem</button>
-  </div>
-</form>
- 
- <hr>
- <?php
- if(isset($_FILES['pic']))
- {
-    $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
-    $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-    $dir = './imagens/'; //Diretório para uploads
- 
-    move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
-    
- } 
- if (isset($_SESSION))
- {
-    $titulo = $_POST["titulo"];
-    $archivo = $_POST["archivo"];
-    $idusuario = $_POST["idusuario"];
-
-    require_once("conectar.php");
-   subirFoto($titulo, $archivo, $idusuario);
-
-    
- }
- 
- ?>
- 
- </div>
-<body>
 </html>
